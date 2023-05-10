810b42e5dc5f4f7057890247d45deca5dcf9425e
Atij Mahesh <atijmahesh914@gmail.com>
2023-05-10 11:09:42 -0700

    Update Positionless_Stats_Analyzer.R

--- Positionless_Stats_Analyzer.R
+++ Positionless_Stats_Analyzer.R
@@ -21,7 +21,7 @@ read_data <- function(year, data_type) {
 yearly_sums <- list()
 
 # Loop over each year
-for (year in 2005:2022) {
+for (year in 1994:2022) {
   # Read in the receiving and rushing data for the current year
   receiving_data_original <- read_data(year, "receiving")
   rushing_data_original <- read_data(year, "rushing")
@@ -48,6 +48,13 @@ for (year in 2005:2022) {
   rb_rushing_data <- rushing_data_original %>%
     filter(Att >= 1, Player != "Player", Pos == "RB")
   
+  fb_rushing_data <- rushing_data_original %>%
+    filter(Att >= 1, Player != "Player", Pos == "FB")
+  
+  wr_rushing_data <- rushing_data_original %>%
+    filter(Att >= 1, Player != "Player", Pos == "WR")
+  
+  
   # Calculate the sums for each column of receiving data
   receiving_data[, c("Rec", "Yds", "TD")] <- lapply(receiving_data[, c("Rec", "Yds", "TD")], as.numeric)
   receiving_sums <- colSums(receiving_data[, c("Rec", "Yds", "TD")], na.rm = TRUE)
@@ -68,9 +75,22 @@ for (year in 2005:2022) {
   rb_rushing_sums <- colSums(rb_rushing_data[, c("Att", "Yds", "TD")], na.rm = TRUE)
   names(rb_rushing_sums) <- c("RB Attempts", "RB Yards", "RB Touchdowns")
   
+  # Calculate the sums for each column of rushing data
+  fb_rushing_data[, c("Att", "Yds", "TD")] <- lapply(fb_rushing_data[, c("Att", "Yds", "TD")], as.numeric)
+  fb_rushing_sums <- colSums(fb_rushing_data[, c("Att", "Yds", "TD")], na.rm = TRUE)
+  names(fb_rushing_sums) <- c("FB Attempts", "FB Yards", "FB Touchdowns")
+  
+  # Calculate the sums for each column of rushing data
+  wr_rushing_data[, c("Att", "Yds", "TD")] <- lapply(wr_rushing_data[, c("Att", "Yds", "TD")], as.numeric)
+  wr_rushing_sums <- colSums(wr_rushing_data[, c("Att", "Yds", "TD")], na.rm = TRUE)
+  names(wr_rushing_sums) <- c("WR Attempts", "WR Yards", "WR Touchdowns")
+  print(year)
+  print(rushing_sums)
+  print(rb_rushing_sums)
+  print(fb_rushing_sums)
+  print(wr_rushing_sums)
   # Store the sums for the current year in a list
-  yearly_sums[[year]] <- list(receiving_sums, rushing_sums, wr_receiving_sums, rb_rushing_sums)
-  print(yearly_sums[[year]])
+  yearly_sums[[year]] <- list(receiving_sums, rushing_sums, wr_receiving_sums, rb_rushing_sums, fb_rushing_sums, wr_rushing_sums)
 }
 
 # Create an empty data frame to store the data
@@ -86,9 +106,15 @@ yearly_wr_tds <- data.frame()
 yearly_rb_att <- data.frame()
 yearly_rb_yds <- data.frame()
 yearly_rb_tds <- data.frame()
+yearly_fb_att <- data.frame()
+yearly_fb_yds <- data.frame()
+yearly_fb_tds <- data.frame()
+yearly_wr_att <- data.frame()
+yearly_wr_rush_yds <- data.frame()
+yearly_wr_rush_tds <- data.frame()
 
 # Loop over each year
-for (year in 2005:2022) {
+for (year in 1994:2022) {
   # Get the sublist for the current year
   nonwr_year_sums <- yearly_sums[[year]][[1]]
   yearly_rec <- rbind(yearly_rec, data.frame(year = year, rec = nonwr_year_sums[1]))
@@ -109,6 +135,16 @@ for (year in 2005:2022) {
   yearly_rb_att <- rbind(yearly_rb_att, data.frame(year = year, att = rb_year_sums[1]))
   yearly_rb_yds <- rbind(yearly_rb_yds, data.frame(year = year, rush_yards = rb_year_sums[2]))
   yearly_rb_tds <- rbind(yearly_rb_tds, data.frame(year = year, rush_tds = rb_year_sums[3]))
+  
+  fb_year_sums <- yearly_sums[[year]][[5]]
+  yearly_fb_att <- rbind(yearly_fb_att, data.frame(year = year, att = fb_year_sums[1]))
+  yearly_fb_yds <- rbind(yearly_fb_yds, data.frame(year = year, rush_yards = fb_year_sums[2]))
+  yearly_fb_tds <- rbind(yearly_fb_tds, data.frame(year = year, rush_tds = fb_year_sums[3]))
+  
+  wr_rush_year_sums <- yearly_sums[[year]][[6]]
+  yearly_wr_att <- rbind(yearly_wr_att, data.frame(year = year, att = wr_rush_year_sums[1]))
+  yearly_wr_rush_yds <- rbind(yearly_wr_rush_yds, data.frame(year = year, rush_yards = wr_rush_year_sums[2]))
+  yearly_wr_rush_tds <- rbind(yearly_wr_rush_tds, data.frame(year = year, rush_tds = wr_rush_year_sums[3]))
 }
 
 
@@ -131,81 +167,104 @@ plot(yearly_rec_tds$year, yearly_rec_tds$rec_tds,
      main = "Non-WR Receiving TDs per Year", xlab = "Year", ylab = "Receiving TDs", type = "l")
 dev.off()
 
-pdf("yearly_att.pdf")
-plot(yearly_att$year, yearly_att$att,
-     main = "Non-RB Rushing Attempts per Year", xlab = "Year", ylab = "Rushing Attempts", type = "l")
+# Create PDF with rushing attempts data
+pdf("yearly_rush_attempts.pdf")
+plot(yearly_att$year, yearly_att$att, type = "l", col = "blue",
+     main = "Non-RB Rushing Attempts per Year", xlab = "Year", ylab = "Rushing Attempts",
+     ylim = c(0, max(yearly_att$att, yearly_fb_att$att, yearly_wr_att$att))) # Set the y-axis limits accordingly
+lines(yearly_fb_att$year, yearly_fb_att$att, col = "red")
+lines(yearly_wr_att$year, yearly_wr_att$att, col = "green")
+legend("topright", legend = c("Non-RB", "Fullback", "Wide Receiver"), col = c("blue", "red", "green"), lty = 1)
 dev.off()
 
-pdf("yearly_rush_yds.pdf")
-plot(yearly_rush_yds$year, yearly_rush_yds$rush_yards,
-     main = "Non-RB Rushing Yards per Year", xlab = "Year", ylab = "Rushing Yards", type = "l")
+# Create PDF with rushing yards data
+pdf("yearly_rush_yards.pdf")
+plot(yearly_rush_yds$year, yearly_rush_yds$rush_yards, type = "l", col = "blue",
+     main = "Non-RB Rushing Yards per Year", xlab = "Year", ylab = "Rushing Yards",
+     ylim = c(0, max(yearly_rush_yds$rush_yards, yearly_fb_yds$rush_yards, yearly_wr_rush_yds$rush_yards))) # Set the y-axis limits accordingly
+lines(yearly_fb_yds$year, yearly_fb_yds$rush_yards, col = "red")
+lines(yearly_wr_rush_yds$year, yearly_wr_rush_yds$rush_yards, col = "green")
+legend("topright", legend = c("Non-RB", "Fullback", "Wide Receiver"), col = c("blue", "red", "green"), lty = 1)
 dev.off()
 
+# Create PDF with rushing touchdowns data
 pdf("yearly_rush_tds.pdf")
-plot(yearly_rush_tds$year, yearly_rush_tds$rush_tds,
-     main = "Non-RB Rushing TDs per Year", xlab = "Year", ylab = "Rushing TDs", type = "l")
+plot(yearly_rush_tds$year, yearly_rush_tds$rush_tds, type = "l", col = "blue",
+     main = "Non-RB Rushing Touchdowns per Year", xlab = "Year", ylab = "Rushing Touchdowns",
+     ylim = c(0, max(yearly_rush_tds$rush_tds, yearly_fb_tds$rush_tds, yearly_wr_rush_tds$rush_tds))) # Set the y-axis limits accordingly
+lines(yearly_fb_tds$year, yearly_fb_tds$rush_tds, col = "red")
+lines(yearly_wr_rush_tds$year, yearly_wr_rush_tds$rush_tds, col = "green")
+legend("topright", legend = c("Non-RB", "Fullback", "Wide Receiver"), col = c("blue", "red", "green"), lty = 1)
 dev.off()
+
 
 # Install required packages if not already installed
 if (!require("tidyverse")) install.packages("tidyverse")
 
-pdf("wr_rec_vs_nonwr_rec")
+pdf("wr_rec_vs_nonwr_rec.pdf")
 ggplot() +
   geom_col(data=yearly_wr_rec, aes(x=year, y=rec, fill="WRs")) +
   geom_col(data=yearly_rec, aes(x=year, y=rec, fill="Non-WRs")) +
   labs(title="Receptions by Position", fill="Position", y="Receptions") +
-  scale_x_continuous(breaks = seq(2005, 2022, by = 2)) +
+  scale_x_continuous(breaks = seq(1994, 2022, by = 2)) +
   scale_fill_manual(values = c("red", "blue"), name = "Position", labels = c("Non-WRs", "WRs")) +
   expand_limits(x = c(2005, 2022))
 dev.off()
 
-pdf("wr_yds_vs_nonwr_yds")
+pdf("wr_yds_vs_nonwr_yds.pdf")
 ggplot() +
   geom_col(data=yearly_wr_yds, aes(x=year, y=rec_yards, fill="WRs")) +
   geom_col(data=yearly_rec_yds, aes(x=year, y=rec_yards, fill="Non-WRs")) +
   labs(title="Receiving Yards by Position", fill="Position", y="Receiving Yards") +
-  scale_x_continuous(breaks = seq(2005, 2022, by = 2)) +
+  scale_x_continuous(breaks = seq(1994, 2022, by = 2)) +
   scale_fill_manual(values = c("red", "blue"), name = "Position", labels = c("Non-WRs", "WRs")) +
   expand_limits(x = c(2005, 2022))
 dev.off()
 
-pdf("wr_tds_vs_nonwr_tds")
+pdf("wr_tds_vs_nonwr_tds.pdf")
 ggplot() +
   geom_col(data=yearly_wr_tds, aes(x=year, y=rec_tds, fill="WRs")) +
   geom_col(data=yearly_rec_tds, aes(x=year, y=rec_tds, fill="Non-WRs")) +
   labs(title="Receiving Touchdowns by Position", fill="Position", y="Receiving Touchdowns") +
-  scale_x_continuous(breaks = seq(2005, 2022, by = 2)) +
+  scale_x_continuous(breaks = seq(1994, 2022, by = 2)) +
   scale_fill_manual(values = c("red", "blue"), name = "Position", labels = c("Non-WRs", "WRs")) +
   expand_limits(x = c(2005, 2022))
 dev.off()
 
-pdf("rb_att_vs_nonrb_att")
+pdf("rb_att_vs_nonrb_att.pdf")
 ggplot() +
   geom_col(data=yearly_rb_att, aes(x=year, y=att, fill="RBs")) +
   geom_col(data=yearly_att, aes(x=year, y=att, fill="Non-RBs")) +
+  geom_col(data=yearly_fb_att, aes(x=year, y=att, fill="FBs")) +
+  geom_col(data=yearly_wr_att, aes(x=year, y=att, fill="WRs")) +
   labs(title="Rushing Attempts by Position", fill="Position", y="Rushing Attempts") +
-  scale_x_continuous(breaks = seq(2005, 2022, by = 2)) +
-  scale_fill_manual(values = c("orange", "blue"), name = "Position", labels = c("RBs", "Non-RBs")) +
+  scale_x_continuous(breaks = seq(1994, 2022, by = 2)) +
+  scale_fill_manual(values = c("orange", "blue", "chartreuse", "magenta"), name = "Position", labels = c("FBs", "Non-RBs", "RBs", "WRs")) +
   expand_limits(x = c(2005, 2022))
 dev.off()
 
-pdf("rb_yds_vs_nonrb_yds")
+pdf("rb_yds_vs_nonrb_yds.pdf")
 ggplot() +
   geom_col(data=yearly_rb_yds, aes(x=year, y=rush_yards, fill="RBs")) +
   geom_col(data=yearly_rush_yds, aes(x=year, y=rush_yards, fill="Non-RBs")) +
+  geom_col(data=yearly_fb_yds, aes(x=year, y=rush_yards, fill="FBs")) +
+  geom_col(data=yearly_wr_rush_yds, aes(x=year, y=rush_yards, fill="WRs")) +
   labs(title="Rushing Yards by Position", fill="Position", y="Rushing Yards") +
-  scale_x_continuous(breaks = seq(2005, 2022, by = 2)) +
-  scale_fill_manual(values = c("orange", "blue"), name = "Position", labels = c("RBs", "Non-RBs")) +
+  scale_x_continuous(breaks = seq(1994, 2022, by = 2)) +
+  scale_fill_manual(values = c("orange", "blue", "chartreuse", "magenta"), name = "Position", labels = c("FBs", "Non-RBs", "RBs", "WRs")) +
   expand_limits(x = c(2005, 2022))
 dev.off()
 
-pdf("rb_tds_vs_nonrb_tds")
+
+pdf("rb_tds_vs_nonrb_tds.pdf")
 ggplot() +
   geom_col(data=yearly_rb_tds, aes(x=year, y=rush_tds, fill="RBs")) +
   geom_col(data=yearly_rush_tds, aes(x=year, y=rush_tds, fill="Non-RBs")) +
+  geom_col(data=yearly_fb_tds, aes(x=year, y=rush_tds, fill="FBs")) +
+  geom_col(data=yearly_wr_rush_tds, aes(x=year, y=rush_tds, fill="WRs")) +
   labs(title="Rushing Touchdowns by Position", fill="Position", y="Rushing Touchdowns") +
-  scale_x_continuous(breaks = seq(2005, 2022, by = 2)) +
-  scale_fill_manual(values = c("orange", "blue"), name = "Position", labels = c("RBs", "Non-RBs")) +
+  scale_x_continuous(breaks = seq(1994, 2022, by = 2)) +
+  scale_fill_manual(values = c("orange", "blue", "chartreuse", "magenta"), name = "Position", labels = c("FBs", "Non-RBs", "RBs", "WRs")) +
   expand_limits(x = c(2005, 2022))
 dev.off()
 
